8517f33310cd70d67ab4044e14051579f7334925
GMKenny <kenny.vandenberg@student.hu.nl>
2021-02-07 17:14:46 +0100

    Spelfouten en Shapiro
    Spelfouten verbeterd en het Shapiro algoritme aangepast zo dat die verdeelt is over 2 functies omdat het algoritme van Knut ongeveer het zelfde zal gaan doen.

--- mastermind_functions.py
+++ mastermind_functions.py
@@ -2,31 +2,37 @@ import random
 import itertools
 
 
-kleuren = ["Rood", "Groen", "Wit", "Zwart", "Blauw", "Geel"]
+colours = ["Rood", "Groen", "Wit", "Zwart", "Blauw", "Geel"]
 
 
-def correct_coller_code(colers):
-    coler_nummbers = []
-    for item in colers:
-        coler_code = kleuren.index(item)
-        coler_nummbers.append(coler_code)
-    return coler_nummbers
+def correct_coller_code(chosen_colers):
+    """"Correct_coler_code loops through every word in the given list and returns a list with the index numbers  """
+    colours_nummbers = []
+    for item in chosen_colers:
+        coler_code = colours.index(item)
+        colours_nummbers.append(coler_code)
+    return colours_nummbers
 
 
 def all_possible_answer():
+    """"All_possible_answer loops trough every item from 0 t/m 5 and returns a list with tuples with all possible combinations"""
     al_codes = list(itertools.product([0, 1, 2, 3, 4, 5], repeat=4))
     return al_codes
 
 
 def random_code(lst):
+    """Random_code retuns a random code from the given list"""
     code_choice = lst[random.randint(0, (len(lst)-1))]
     return code_choice
 
 
 def guessing_feedback(secret_code, guess_list):
+    """"Given the secret code and guess determine how many colours and positions are correct. Create a list copy of
+    the given secret and guess tuples. Loop though the items and determine if the item is the same colour and position,
+    if true add one to the correct pins and remove current item form the copy lists.
+    Loop trough the items that are left and determine if the guess colours are in the secret code. """
     position_correct = 0
     color_correct = 0
-    # Zonder copy word er 1 een color bij opgeteld of vergeten als dat niet moet.
     copy_secret = list(secret_code[:])
     copy_guess = list(guess_list[:])
     for index in range(0, 4):
@@ -41,18 +47,29 @@ def guessing_feedback(secret_code, guess_list):
     return position_correct, color_correct
 
 
+def reduce_code_list(all_possible_list, guess, guess_feedback):
+    """Given the list with current possibilities, guess and feedback determine what answers are left.
+     Loop trough the every item in de list and determine if the current item would have created the same feedback as the
+     current feedback. if true add the item to a new list of codes"""
+    current_list = []
+    for item in all_possible_list:
+        if guessing_feedback(guess, item) == guess_feedback:
+            current_list.append(item)
+    return current_list
+
+
 def simpel_algorithme(secret):
+    """Given the secret code return list with all decisions made by the computer. Create a list of all possible answers.
+    Loop 6 times (The maximum needed to solve the the secret) and create a random guess given the possible answers.
+    Receive feedback on the guess and add the guess and feedback in a decision list,
+    receive a list of possible answers based on the feedback. if the guess equal to the secret return all decisions"""
     all_possible_list = all_possible_answer()
     all_decisions = []
-    for i in range(10):
+    for i in range(6):
         guess = random_code(all_possible_list)
         guess_feedback = guessing_feedback(guess, secret)
         all_decisions.append([guess, guess_feedback])
-        current_list = []
-        for item in all_possible_list:
-            if guessing_feedback(guess, item) == guess_feedback:
-                current_list.append(item)
-        all_possible_list = current_list
+        all_possible_list = reduce_code_list(all_possible_list, guess, guess_feedback)
         if list(guess) == secret:
             break
     return all_decisions
--- mastermind_interface.py
+++ mastermind_interface.py
@@ -1,38 +1,39 @@
 import mastermind_functions as mf
 
 def game_text(num):
-    rules = ["Kies in de main menu welke optie je wilt met 1, 2, 3, 4\nInput 1: Een code kraken, de computer bedenkt"
-              "een code en je moet de code van de computer kraken\nInput 2: een code bedenken, je vult een secret code "
-              "in en de computer gokt met verschilende algoritmes naar het antwoord \nInput 3: Spelregels "
-              "\nInput 4: Stoppen\nJe kan uit de volgende kleuren kiezen\n", *mf.kleuren]
-    return print(rules[num])
+    tekst = ["Kies in de main menu welke opties je wilt met 1, 2, 3, 4", "1: Een code kraken, de computer bedenkt"
+              " een code en je moet de code van de computer kraken.\n2: een code bedenken, je vult een secret code "
+              "in en de computer gokt met verschillende algoritmes naar het antwoord \n3: Spelregels "
+              "\n4: Stoppen\nJe kan uit de volgende kleuren kiezen", [*mf.colours],
+             "1, Een code kraken\n2, Een code bedenken\n3, Spelregels\n4, Stoppen"]
+    return tekst[num]
 
 
 def main_menu():
     while True:
         print("Mastermind")
-        print("1, Een code kraken", "\n2, Een code bedenken", "\n3, Spelregels", "\n4, Stoppen")
-        input_game = str(input())
+        print(game_text(0))
+        print(game_text(3))
+        input_game = str(input("Keuze:"))
         if input_game == '1':
             game_kraken()
         elif input_game == "2":
-            return
+            show_simpel_algorithme_results()
         elif input_game == "3":
-            game_text(0)
-            game_text(1)
-            return
+            print(game_text(1))
+            print(*game_text(2), sep=" ")
         elif input_game == "4":
             break
 
 
 def input_code():
-    coler_list = []
+    colours_list = []
     for i in range(1, 5):
         code_input = ""
-        while code_input not in mf.kleuren:
+        while code_input not in mf.colours:
             code_input = input("Pin {}:".format(i))
-        coler_list.append(code_input)
-    code_list = mf.correct_coller_code(coler_list)
+        colours_list.append(code_input)
+    code_list = mf.correct_coller_code(colours_list)
     return code_list
 
 
@@ -40,7 +41,7 @@ def game_kraken():
     code_list = mf.all_possible_answer()
     secret_code = mf.random_code(code_list)
     counter = 11
-    print("Kies uit de volgende kleuren:", *mf.kleuren)
+    print("Kies uit de volgende kleuren:", *mf.colours)
     while counter > 0:
         print("Nog", counter, "gokken te gaan")
         counter -= 1
@@ -53,6 +54,23 @@ def game_kraken():
             print("Goede plek:", position_correct)
             print("Correcte kleur:", color_correct)
     print("Game over")
+
+
+def show_simpel_algorithme_results():
+    print("Kies uit de volgende kleuren:", *mf.colours)
+    secret = input_code()
+    answers = mf.simpel_algorithme(secret)
+    colours_and_pegs = []
+    for item in answers:
+        colour_string = ""
+        for colour in item[0]:
+            colour_string += str(mf.colours[colour]) + " "
+        colours_and_pegs.append(colour_string)
+        correct_position = "Correct position: " + str(item[1][0]) + " Colour correct: " + str(item[1][1])
+        colours_and_pegs.append(correct_position)
+    for item in colours_and_pegs:
+        print(item)
+    return
 
 
 main_menu()
